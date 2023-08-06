acaf7afecae146f1c3ef24f27ddebcab0fb38374
ROHITH D N <103078929+RohithNagesh@users.noreply.github.com>
2023-08-06 15:01:27 +0530

    Create inventory.c

---
+++ inventory.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "inventory.h"
+
+struct Product inventory[MAX_PRODUCTS];
+int num_products = 0;
+
+void addProduct() {
+}
+
+void displayInventory() {
+}
+
+void saveInventory() {
+}
+
+void loadInventory() {
+}
+
+int main() {
+}
